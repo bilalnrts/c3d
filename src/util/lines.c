35f5d77686a1ded1e21a20bed6d13158b4275d94
Bilal Salih Nurtas <bsalihnrts@gmail.com>
2023-08-19 02:06:48 +0300

    created some functions related to rows -- I'll use later

---
+++ src/util/lines.c
@@ -0,0 +1,58 @@
+#include "../../inc/cub3.h"
+
+int	ft_line_lenght(char *line)
+{
+	int	i;
+	int total;
+
+	i = 0;
+	total = 0;
+	while (line[i])
+	{
+		if (line[i] == '\t')
+			total += 4;
+		else
+			total++;
+		i++;
+	}
+	return (total);
+}
+
+char	*modificate_line(char *line)
+{
+	int		i;
+	int		j;
+	int		limit;
+	char	*new_line;
+
+	i = 0;
+	j = 0;
+	new_line = malloc(ft_line_lenght(line) + 1);
+	while (line[i])
+	{
+		if (line[i] == '\t')
+		{
+			limit = 4;
+			while (limit)
+			{
+				new_line[j] = '2';
+				limit--;
+				j++;
+			}
+		}
+		else if (line[i] == ' ')
+		{
+			new_line[j] = '2';
+			j++;
+		}
+		else
+		{
+			new_line[j] = line[i];
+			j++;
+		}
+		i++;
+	}
+	new_line[j] = '\0';
+	free(line);
+	return (new_line);
+}
