be55274cd7606c0556512bc0cf384136a77a93c3
Bilal Salih Nurtas <bsalihnrts@gmail.com>
2023-08-18 17:18:00 +0300

    new variables added

--- inc/cub3.h
+++ inc/cub3.h
@@ -1,17 +1,32 @@
 #ifndef CUB3_H
 # define CUB3_H
 
-#include "../lib/libft/inc/libft.h"
-#include "../lib/libft/inc/ft_printf.h"
-#include "../lib/libft/inc/get_next_line.h"
-#include <unistd.h>
-#include <stdlib.h>
-#include <fcntl.h>
-#include <math.h>
+# include "../lib/libft/inc/libft.h"
+# include "../lib/libft/inc/ft_printf.h"
+# include "../lib/libft/inc/get_next_line.h"
+# include <unistd.h>
+# include <stdlib.h>
+# include <fcntl.h>
+# include <math.h>
+
+enum	e_direction
+{
+	NORTH,
+	SOUTH,
+	WEST,
+	EAST
+};
 
 typedef struct s_map
 {
 	char	**map;
+	char	*no_texture;
+	char	*so_texture;
+	char	*we_texture;
+	char	*ea_texture;
+	char	*f_color_rgb;
+	char	*c_color_rgb;
+	enum e_direction	start_position;
 	int		map_width;
 	int		map_height;
 }				t_map;
