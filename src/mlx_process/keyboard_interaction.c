b6484844dad956854c80a325a9827c580374ee92
Anıl Dervişoğlu <anildervisoglu@gmail.com>
2023-09-12 15:57:48 +0300

    necessary keyboard interactions are processed here

---
+++ src/mlx_process/keyboard_interaction.c
@@ -0,0 +1,45 @@
+#include "../../inc/cub3.h"
+
+int ft_press_key(int key, t_map *map)
+{
+    if (key == 13)
+        map->player->move_no = 1;
+    else if (key == 1)
+        map->player->move_so = 1;
+    else if (key == 0)
+        map->player->move_we = 1;
+    else if (key == 2)
+        map->player->move_ea = 1;
+    else if (key == 123)
+        map->player->turn = LEFT;
+    else if (key == 124)
+        map->player->turn = RIGHT;
+    else if (key == 53)
+        ft_close(key, map);
+    return (0);
+}
+
+int ft_release_key(int key, t_map *map)
+{
+    if (key == 13)
+        map->player->move_no = 0;
+    else if (key == 1)
+        map->player->move_so = 0;
+    else if (key == 0)
+        map->player->move_we = 0;
+    else if (key == 2)
+        map->player->move_ea = 0;
+    else if (key == 123)
+        map->player->turn = NONE;
+    else if (key == 124)
+        map->player->turn = NONE;
+    return (0);
+}
+
+int ft_close(int key, t_map *map)
+{
+    (void)key;
+    (void)map;
+    exit(0);
+    return (0);
+}
